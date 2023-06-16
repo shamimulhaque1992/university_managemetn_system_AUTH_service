5ab69f09c3b03b016fe2e7fe0d3a7bbb6eb47285
shamimulhaque1992 <khandoker15-1992@diu.edu.bd>
2023-06-17 02:33:28 +0600

    [Done]service, controller, route for user is done

---
+++ src/app/modules/users/users.route.ts
@@ -0,0 +1,6 @@
+import express from 'express'
+import usersController from './users.controller'
+const router = express.Router()
+
+router.post('/create-user', usersController.createUser)
+export default router
--- src/config/index.ts
+++ src/config/index.ts
@@ -5,5 +5,5 @@ dotenv.config({ path: path.join(process.cwd(), '.env') })
 export default {
   port: process.env.PORT,
   database_url: process.env.DATABASE_URL,
-  default_student_password: process.env.DEFAULT_STUDENT_PASSWORD,
+  default_user_password: process.env.DEFAULT_USER_PASSWORD,
 }
