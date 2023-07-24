0a103f9a22ddec0a359ddbee5ede069e2b9f87a4
shamimulhaque1992 <khandoker15-1992@diu.edu.bd>
2023-07-24 22:46:44 +0600

    [Done] some data changed

--- src/app.ts
+++ src/app.ts
@@ -1,22 +1,22 @@
-import express, { Application } from 'express'
-import cors from 'cors'
-import globalErrorHandler from './app/middlewares/globalErrorHandler'
-import { UserRoutes } from './app/modules/user/user.route'
+import express, { Application } from 'express';
+import cors from 'cors';
+import globalErrorHandler from './app/middlewares/globalErrorHandler';
+import { UserRoutes } from './app/modules/user/user.route';
 
-const app: Application = express()
+const app: Application = express();
 
-app.use(cors())
-app.use(express.json())
-app.use(express.urlencoded({ extended: true }))
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 // Application routes
-app.use('/api/v1/users', UserRoutes)
+app.use('/api/v1/users', UserRoutes);
 
 // Testing root is working
 /* app.get('/', async (req: Request, res: Response, next: NextFunction) => {
   throw new Error("Testing Error Logger")
 }) */
 
-app.use(globalErrorHandler)
+app.use(globalErrorHandler);
 
-export default app
+export default app;
--- src/app/modules/user/user.service.ts
+++ src/app/modules/user/user.service.ts
@@ -1,26 +1,26 @@
-import config from '../../../config/index'
-import ApiError from '../../../errors/ApiError'
-import { IUser } from './user.interface'
-import { User } from './user.model'
-import { generateUserId } from './user.utils'
+import config from '../../../config/index';
+import ApiError from '../../../errors/ApiError';
+import { IUser } from './user.interface';
+import { User } from './user.model';
+import { generateUserId } from './user.utils';
 
 const createUser = async (user: IUser): Promise<IUser | null> => {
   // auto generated incremental id
-  const id = await generateUserId()
+  const id = await generateUserId();
 
-  user.id = id
+  user.id = id;
   // default password
   if (!user.password) {
-    user.password = config.default_user_password as string
+    user.password = config.default_user_password as string;
   }
 
-  const createdUser = await User.create(user)
+  const createdUser = await User.create(user);
   if (!createUser) {
-    throw new ApiError(4000, 'Failed to create user!')
+    throw new ApiError(4000, 'Failed to create user!');
   }
-  return createdUser
-}
+  return createdUser;
+};
 
 export const UserService = {
   createUser,
-}
+};
