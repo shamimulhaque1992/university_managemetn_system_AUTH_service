2230eb8cd787a3967e884e2f75c4cf5d8022ab7f
shamimulhaque1992 <khandoker15-1992@diu.edu.bd>
2024-01-28 02:49:27 +0600

    successfully hosted in vercel

--- src/app/middlewares/globalErrorHandler.ts
+++ src/app/middlewares/globalErrorHandler.ts
@@ -1,78 +1,7 @@
-// /* eslint-disable no-console */
-// /* eslint-disable no-unused-expressions */
-// import { ErrorRequestHandler, Request, Response } from 'express';
-// import config from '../../config';
-// import { IGenericErrorMessage } from '../../interfaces/error';
-// import handelValidationError from '../../errors/handleValidationError';
-// import { Error } from 'mongoose';
-// import ApiError from '../../errors/ApiError';
-// import { errorLogger } from '../../shared/logger';
-// import { ZodError } from 'zod';
-// import handleZodError from '../../errors/handleZodError';
-// import handelCastError from '../../errors/handleCastError';
-
-// const globalErrorHandler: ErrorRequestHandler = (
-//   error,
-//   req: Request,
-//   res: Response
-// ) => {
-//   config.env === 'development'
-//     ? console.log('💀 globalErrorHandler~', error)
-//     : console.log('💀 globalErrorHandler~', error);
-//   let statusCode = 500;
-//   let message = 'Something went wrong!';
-//   let errorMessages: IGenericErrorMessage[] = [];
-
-//   if (error?.name === 'ValidationError') {
-//     const simplifiedError = handelValidationError(error);
-//     statusCode = simplifiedError.statusCode;
-//     message = simplifiedError.message;
-//     errorMessages = simplifiedError.errorMessages;
-//   } else if (error instanceof ZodError) {
-//     const simplifiedError = handleZodError(error);
-//     statusCode = simplifiedError.statusCode;
-//     message = simplifiedError.message;
-//     errorMessages = simplifiedError.errorMessages;
-//   } else if (error?.name === 'CastError') {
-//     const simplifiedError = handelCastError(error);
-//     statusCode = simplifiedError.statusCode;
-//     message = simplifiedError.message;
-//     errorMessages = simplifiedError.errorMessages;
-//   } else if (error instanceof ApiError) {
-//     statusCode = error?.statusCode;
-//     message = error?.message;
-//     errorMessages = error?.message
-//       ? [
-//           {
-//             path: '',
-//             message: error.message,
-//           },
-//         ]
-//       : [];
-//   } else if (error instanceof Error) {
-//     message = error?.message;
-//     errorMessages = error?.message
-//       ? [
-//           {
-//             path: '',
-//             message: error?.message,
-//           },
-//         ]
-//       : [];
-//   }
-//   res.status(statusCode).json({
-//     success: false,
-//     message,
-//     errorMessages,
-//     stack: config.env !== 'production' ? error?.stack : undefined,
-//   });
-// };
-// export default globalErrorHandler;
-
 /* eslint-disable no-unused-vars */
 /* eslint-disable no-console */
 /* eslint-disable no-unused-expressions */
-import { ErrorRequestHandler, Request, Response } from 'express';
+import { ErrorRequestHandler, NextFunction, Request, Response } from 'express';
 import config from '../../config';
 import ApiError from '../../errors/ApiError';
 import handleValidationError from '../../errors/handleValidationError';
@@ -81,12 +10,12 @@ import { ZodError } from 'zod';
 import handleCastError from '../../errors/handleCastError';
 import handleZodError from '../../errors/handleZodError';
 import { IGenericErrorMessage } from '../../interfaces/error';
-import { errorLogger } from '../../shared/logger';
 
 const globalErrorHandler: ErrorRequestHandler = (
   error,
   req: Request,
-  res: Response
+  res: Response,
+  next: NextFunction
 ) => {
   config.env === 'development'
     ? console.log(`🐱‍🏍 globalErrorHandler ~~`, { error })
@@ -143,3 +72,10 @@ const globalErrorHandler: ErrorRequestHandler = (
 };
 
 export default globalErrorHandler;
+
+//path:
+//message:
+
+// 2025 Fall
+
+// 2025 and
